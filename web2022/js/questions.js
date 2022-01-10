ab11125813896f40a44555922ba5f2fe12d44984
aminatd <94138060+aminatd@users.noreply.github.com>
2022-01-11 00:13:00 +0600

    Add files via upload

---
+++ web2022/js/questions.js
@@ -0,0 +1,167 @@
+ let questions = [
+     {
+         numb: 1,
+         question: "What does HTML stand for?",
+         answer: "Hypertext Markup Language",
+         options:[
+             "Hard To Make Love",
+             "Hypertext Markup Language",
+             "Hey That's My Lunch",
+             "Hyper Tool Markup Language"
+         ]
+     },
+     {
+        numb: 2,
+        question: "What is the biggest lie in computer programming?",
+        answer: "HTML is a programming language",
+        options:[
+            "It should work now",
+            "I solved a bug",
+            "HTML is a programming language",
+            "Oh it's gonna be easy"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What is the greatest fear of programmers ?",
+        answer: "When they forget to save.",
+        options:[
+            "BUGs! BUGs! BUGs everywhere :/",
+            "Deploy on Friday evening.",
+            "When they forget to save.",
+            "When test fails when run together but passes alone."
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which is the most used language for web development?",
+        answer: "JavaScript",
+        options:[
+            "JavaScript",
+            "Human language",
+            "Java",
+            "HTML"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Oct 31 = Dec 25 ?",
+        answer: "Octal 31 is equel to Decimal 25",
+        options:[
+            "Halloween = Christmas",
+            "Octal 31 is equel to Decimal 25",
+            "Not equal",
+            "None"
+        ]
+    },
+    {
+        numb: 6,
+        question: "What is a web developer's favorite snack?",
+        answer: "Cookies",
+        options:[
+            "Chocolate",
+            "Coffee",
+            "All the snacks",
+            "Cookies"
+        ]
+    },
+    {
+        numb: 7,
+        question: "What does SEO stand for?",
+        answer: "Search Engine Optimization",
+        options:[
+            "Sure Enjoy Optimizing",
+            "Silence Everyones Opinion",
+            "Search Engine Optimization",
+            "Search Everything Online"
+        ]
+    },
+    {
+        numb: 8,
+        question: "IsNaN() Evaluates And Argument To Determine if Given Value:",
+        answer: "Is Not a numb",
+        options:[
+            "Is Not a numb",
+            "Is Not a Null",
+            "Is Not a New Object",
+            "Is Not a New"
+        ]
+    },
+    {
+        numb: 9,
+        question: "What is a WTF?",
+        answer: "Type of parent theme",
+        options:[
+            "Where's the Faith",
+            "Walk To Freedom",
+            "Type of parent theme",
+            "Web Task Force"
+        ]
+    },
+    {
+        numb: 10,
+        question: "What ia an API?",
+        answer: "A software intermediary that allows two applications to talk to each other.",
+        options:[
+            "A software intermediary that allows two applications to talk to each other.",
+            "A database",
+            "A Framework",
+            "An Application"
+        ]
+    },
+    {
+        numb: 11,
+        question: "What is ReactJS?",
+        answer: "A Library for building interaction interfaces",
+        options:[
+            "User-interface framework",
+            "Server-side Framework",
+            "A Library for building interaction interfaces",
+            "None"
+        ]
+    },
+    {
+        numb: 12,
+        question: "What of the following API is a MUST for every ReactJS component?",
+        answer: "renderComponent",
+        options:[
+            "renderComponent",
+            "getInitialState",
+            "render",
+            "None"
+        ]
+    },
+    {
+        numb: 13,
+        question: "What does the 'webpack' command do?",
+        answer: "A module bundler",
+        options:[
+            "A module bundler",
+            "Deliver free web pages",
+            "Web pack collector",
+            "Runs react local development server"
+        ]
+    },
+    {
+        numb: 14,
+        question: "What is GitHub?",
+        answer: "a code hosting platform for version-control and collaboration platform",
+        options:[
+            "a code hosting platform for version-control and collaboration platform",
+            "a social development environment for front-end designers",
+            "a zero-setup, collaborative, browser-based coding editor",
+            "None"
+        ]
+    },
+    {
+        numb: 15,
+        question:"What is code hoisting?",
+        answer: "All",
+        options:[
+            "the default behavior of moving all the declarations at the top of the scope before code execution.",
+            "lift, raise",
+            "JavaScript's default behavior of moving all declarations to the top of the current scope",
+            "All"
+        ]
+    },
+ ];
